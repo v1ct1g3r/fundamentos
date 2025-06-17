9e23bc5d2f0e23e06b8a6446d7674c26bea5b7ac
Víctor Olmo <victorolmo42@gmail.com>
2025-06-16 22:51:26 -0400

    Primer Parcial-1

---
+++ promedio_de_notas.py
@@ -0,0 +1,24 @@
+print("""
+      --CALCULADORA DE PROMEDIO DE NOTAS--
+      """)
+
+while True:
+    nombre = input("Nombre del estudiante: ")
+    matricula = input("Matrícula: ")
+    notas = []
+
+    for i in range(4):
+        nota = input(f"Nota {i + 1}: ")
+        while float(nota) < 0 or float(nota) > 100 or not nota.isdigit():
+            print("La nota solo puede ser un número mayor o igual que 0 y menor o igual que 100")
+            nota = input(f"Nota {i + 1}: ")
+        notas.append(float(nota))
+
+    promedio = sum(notas) / 4
+    print(f"El promedio de las notas de {nombre} es {promedio}")
+    
+    salida = input("¿Desea calcular el promedio de otro estudiante? (S/N): ").upper()
+    if salida == "S":
+        continue
+    elif salida == "N":
+        break

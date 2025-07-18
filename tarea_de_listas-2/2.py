61592c54dde3e7fbaa37d9bc6127f8a14422e74d
Víctor Olmo <victorolmo42@gmail.com>
2025-07-18 17:10:54 -0400

    Tarea de listas - 2

--- tarea_de_listas-2/2.py
+++ tarea_de_listas-2/2.py
@@ -1,8 +1,17 @@
-"""2. Leer 10 enteros, almacenarlos en una lista y determinar en qué posición de del arreglo está el mayor número par leído."""
+"""2. Leer 10 enteros, almacenarlos en una lista y determinar en qué posición del arreglo está el mayor número par leído."""
 
 numeros = list(map(int, input("Ingrese 10 enteros separados por espacios: ").split()))
-max_par = numeros[0]
-for i in range(1, len(numeros)):
-    if numeros[i] > max_par and numeros[i] % 2 == 0:
-        max_par = numeros[i]
-print(f"El mayor número par está en la posición {numeros.index(max_par)} (del 0 al 9).")
+for num in numeros:
+    if num % 2 == 0:
+        max_par = num
+        break
+else:
+    max_par = "n/a"
+
+if max_par == "n/a":
+    print("No se ingresó ningún número par.")
+else:
+    for num in numeros:
+        if num > max_par and num % 2 == 0:
+            max_par = num
+    print(f"El mayor número par está en la posición {numeros.index(max_par)} (del 0 al 9).")
